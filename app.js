4824721d08579edd3d8bfc2bd0dbc6a6087acf6e
brayanlee <49029472+brayanlee@users.noreply.github.com>
2021-01-24 16:17:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('AWS exercise의 A project입니다.');
-});
+  res . send ( 'Welcome to AWS nginx web-server project!' ) ;
+} ) ;
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
+  console.log('AWS nginx web-serverv app listening on port 3000!');
+} ) ;
 
 app.get('/health', (req, res) => {
   res.status(200).send();
-});
+} ) ;
